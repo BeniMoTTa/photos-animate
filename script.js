444477b0ca797741fbc794e37c332ef185085103
Jozhué Beni <jbeeniif@gmail.com>
2023-10-10 16:51:15 -0300

    Created all section and Header on class 1

---
+++ script.js
@@ -0,0 +1,7 @@
+function showDescription(description) {
+  document.getElementById("image-description").textContent = description;
+}
+
+function hideDescription() {
+  document.getElementById("image-description").textContent = "";
+}
