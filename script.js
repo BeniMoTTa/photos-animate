087c6befcd16a7637491f4ef0f69de69dca8c16a
Jozhué Beni <jbeeniif@gmail.com>
2023-10-11 17:42:40 -0300

    Section 5 finished

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function showDescription(description) {
 function hideDescription() {
   document.getElementById("image-description").textContent = "";
 }
+
+const cards = document.querySelectorAll(".card-five");
+
+cards.forEach((card) => {
+  const backTitle = card.querySelector(".back-title");
+  const contentText = card.querySelector(".text-five");
+
+  card.addEventListener("mouseenter", () => {
+    card.classList.add("card-color-hovered");
+    backTitle.classList.add("back-title-hovered");
+    contentText.classList.add("text-content-hovered");
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.classList.remove("card-color-hovered");
+    backTitle.classList.remove("back-title-hovered");
+    contentText.classList.remove("text-content-hovered");
+  });
+});
